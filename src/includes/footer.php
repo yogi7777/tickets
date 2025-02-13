823350dbebee98dbe3e0062c857c7677db7520fc
yogi7777 <79504934+yogi7777@users.noreply.github.com>
2025-02-13 21:28:46 +0100

    Create footer.php

---
+++ src/includes/footer.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <div class="row align-items-center">
+        <div class="col-auto">
+            <button id="theme-toggle" class="btn btn-link nav-link" onclick="window.darkMode.toggleTheme()">⚙️</button>
+        </div>
+        <div class="col text-center">
+            <p class="mb-0">&copy; <?php echo date('Y'); ?> <a href="https://itcrm.ch" target="_blank">itcrm.ch</a> All rights reserved.</p>
+        </div>
+    </div>
+</div>
